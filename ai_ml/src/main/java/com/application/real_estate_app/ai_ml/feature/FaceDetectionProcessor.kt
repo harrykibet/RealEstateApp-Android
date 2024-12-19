3dae3d4eeb59568b37ae9ceed4a098ea32fa7c5b
harrykibet <trmnjames@gmail.com>
2024-12-19 20:49:35 +0300

    Merge remote-tracking branch 'origin/main'

--- ai_ml/src/main/java/com/application/real_estate_app/ai_ml/feature/FaceDetectionProcessor.kt
+++ ai_ml/src/main/java/com/application/real_estate_app/ai_ml/feature/FaceDetectionProcessor.kt
@@ -1,11 +1,4 @@
-package com.application.real_estate_app.machine_learning.feature
-
-import com.application.real_estate_app.machine_learning.core.MLKitProcessor
-import com.google.mlkit.vision.common.InputImage
-import com.google.mlkit.vision.face.Face
-import com.google.mlkit.vision.face.FaceDetection
-import com.google.mlkit.vision.face.FaceDetectorOptions
-import kotlinx.coroutines.tasks.await
+package com.application.real_estate_app.ai_ml.feature
 
 class FaceDetectionProcessor {
 }
--- ai_ml/src/main/java/com/application/real_estate_app/ai_ml/util/ImageUtils.kt
+++ ai_ml/src/main/java/com/application/real_estate_app/ai_ml/util/ImageUtils.kt
@@ -1,8 +1,4 @@
-package com.application.real_estate_app.machine_learning.util
-
-import android.graphics.Bitmap
-import android.media.Image
-import com.google.mlkit.vision.common.InputImage
+package com.application.real_estate_app.ai_ml.util
 
 object ImageUtils {
 }
