f5a416980da8df4153610349268b90494d23478c
harrykibet <trmnjames@gmail.com>
2025-01-09 00:15:57 +0300

    Merge branch 'main' of https://gitlab.com/android2485351/RealEstateApp

--- feature_profile/src/main/java/com/application/real_estate_app/feature_profile/di/ProfileApiModule.kt
+++ feature_profile/src/main/java/com/application/real_estate_app/feature_profile/di/ProfileApiModule.kt
@@ -1,19 +1,7 @@
 package com.application.real_estate_app.feature_profile.di
 
-import com.application.real_estate_app.feature_profile.data.apis.ProfileApi
-import com.application.real_estate_app.feature_profile.domain.interfaces.IProfileApi
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
-@Module
-@InstallIn(SingletonComponent::class)
+
+
 abstract class ProfileApiModule {
-
-    //Bind ProfileApi to IProfileApi interface
-    @Binds
-    @Singleton
-    abstract fun bindProfileApi(api: ProfileApi) : IProfileApi
 }
