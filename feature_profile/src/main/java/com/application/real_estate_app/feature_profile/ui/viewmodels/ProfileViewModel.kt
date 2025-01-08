af449e2c259c12869363871af0aef8ae3477d7f5
harrykibet <trmnjames@gmail.com>
2025-01-08 22:05:50 +0300

    Merge branch 'main' of https://gitlab.com/android2485351/RealEstateApp

--- feature_profile/src/main/java/com/application/real_estate_app/feature_profile/ui/viewmodels/ProfileViewModel.kt
+++ feature_profile/src/main/java/com/application/real_estate_app/feature_profile/ui/viewmodels/ProfileViewModel.kt
@@ -1,13 +1,13 @@
 package com.application.real_estate_app.feature_profile.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
-import com.application.real_estate_app.domain.interfaces.IUserRepository
+import com.application.real_estate_app.feature_profile.domain.interfaces.IProfileApi
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
-    @Suppress("unused") private val userRepository: IUserRepository
+    @Suppress("unused") private val api: IProfileApi
 ) : ViewModel() {
     // Your ViewModel logic here
 }
