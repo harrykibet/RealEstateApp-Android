a9dad728e5b38d751199d392d16d5322768f6215
harrykibet <trmnjames@gmail.com>
2025-02-06 21:31:32 +0300

    new changes

--- core/src/main/java/com/application/real_estate_app/core/di/SystemModule.kt
+++ core/src/main/java/com/application/real_estate_app/core/di/SystemModule.kt
@@ -8,6 +8,7 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Singleton
 import android.content.Context
+import android.hardware.display.DisplayManager
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -17,5 +18,11 @@ object SystemModule {
     @Singleton
     fun providePowerManager(@ApplicationContext context: Context): PowerManager {
         return context.getSystemService(Context.POWER_SERVICE) as PowerManager
+    }
+
+    @Provides
+    @Singleton
+    fun provideDisplayManager(@ApplicationContext context: Context): DisplayManager {
+        return context.getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
     }
 }
