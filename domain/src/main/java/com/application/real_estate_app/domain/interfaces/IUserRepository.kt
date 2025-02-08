c03435f94890cc47cc482d7ffbd56edd9e1137d5
harrykibet <trmnjames@gmail.com>
2025-02-08 22:04:58 +0300

    new changes

--- domain/src/main/java/com/application/real_estate_app/domain/interfaces/IUserRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.application.real_estate_app.domain.interfaces
-
-import com.application.real_estate_app.domain.models.User
-
-interface IUserRepository {
-    fun createUserIfNotExists(userId: String?, user: User)
-}
