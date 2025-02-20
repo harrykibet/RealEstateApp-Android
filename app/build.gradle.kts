eaf6cfbafba9fc96899cfb98080a85448d034c94
harrykibet <trmnjames@gmail.com>
2025-02-20 19:13:19 +0300

    new changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,7 @@ plugins {
     id("org.jetbrains.kotlin.kapt")
     // Dagger Hilt for Dependency Injection
     id("com.google.dagger.hilt.android")
+    id("androidx.compose.compiler")
     id("org.jetbrains.dokka")
     id("org.sonarqube")
 }
