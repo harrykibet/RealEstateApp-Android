08ffbac1d3993e42ec80040ad43bf6cd15b112ac
harrykibet <trmnjames@gmail.com>
2025-02-24 12:14:58 +0300

    new changes

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `kotlin-dsl` // Enables Kotlin for buildSrc
-    kotlin("jvm") version "1.9.10" // Match this with your project Kotlin version
+    kotlin("jvm") version "2.0.0" // Match this with your project Kotlin version
     id("org.sonarqube") version "6.0.1.5171" // Use the latest version of SonarQube plugin
 }
 
