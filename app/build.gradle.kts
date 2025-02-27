b18e0b121bc03c97cb7e8a5d86a37fc1c59c921f
harrykibet <trmnjames@gmail.com>
2025-02-27 22:55:22 +0300

    new changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 
     defaultConfig {
         applicationId = "com.application.real_estate_app"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
