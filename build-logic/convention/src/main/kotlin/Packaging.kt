c7323cee28b5506b127cd96259396d3af767fa96
harrykibet <trmnjames@gmail.com>
2025-03-10 21:45:14 +0300

    new changes

---
+++ build-logic/convention/src/main/kotlin/Packaging.kt
@@ -0,0 +1,13 @@
+/**
+ * Object containing packaging-related configurations.
+ */
+object Packaging {
+    val excludes = listOf(
+        "META-INF/DEPENDENCIES",
+        "META-INF/INDEX.LIST",
+        "META-INF/versions/9/OSGI-INF/MANIFEST.MF",
+        "META-INF/versions/11/OSGI-INF/MANIFEST.MF",
+        "META-INF/COPYRIGHT.txt",
+        "/META-INF/{AL2.0,LGPL2.1}"
+    )
+}
