7f2316d2295f283a3f946e1585a0256ead7e360c
harrykibet <trmnjames@gmail.com>
2025-03-10 21:45:14 +0300

    new changes

---
+++ build-logic/convention/src/main/kotlin/AndroidConfig.kt
@@ -0,0 +1,54 @@
+/**
+ *  `AndroidConfig` is an object that centralizes all the configuration settings
+ *  for the Android application.
+ *  This includes SDK versions, application identifiers, testing configurations,
+ *  code obfuscation rules, and namespace definitions.
+ *  It serves as a single source of truth for all Android-related build settings,
+ *  improving consistency and maintainability.
+ */
+@Suppress("constPropertyName", "MemberVisibilityCanBePrivate")
+object AndroidConfig {
+
+    // SDK and App Config
+    const val compileSdk = 35
+    const val minSdk = 26
+    const val targetSdk = compileSdk
+    const val applicationId = "com.application.real_estate_app"
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val jvmTarget = "17"
+
+    // Testing
+    const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+    // Dokka
+    const val dokkaPath = "dokka"
+
+    // Proguard Files
+    const val proguardOptimizationFile = "proguard-android-optimize.txt"
+    const val proguardRulesFile = "proguard-rules.pro"
+    const val proguardConsumerRulesFile = "consumer-rules.pro"
+
+    // Namespaces
+    const val appNamespace = applicationId
+    const val coreNamespace = "$appNamespace.core"
+    const val featureAnalyticsNamespace = "$appNamespace.feature_analytics"
+    const val featureChatsNamespace = "$appNamespace.feature_chats"
+    const val featureCommentsNamespace = "$appNamespace.feature_comments"
+    const val featureFavoritesNamespace = "$appNamespace.feature_favorites"
+    const val featureHomeNamespace = "$appNamespace.feature_home"
+    const val featureProfileNamespace = "$appNamespace.feature_profile"
+    const val featureSearchNamespace = "$appNamespace.feature_search"
+    const val featureSettingsNamespace = "$appNamespace.feature_settings"
+    const val uiComponentsNamespace = "$appNamespace.ui_components"
+    const val featureAuthNamespace = "$appNamespace.feature_auth"
+    const val featureIntelligenceNamespace = "$appNamespace.feature_intelligence"
+    const val featureMarketplaceNamespace = "$appNamespace.feature_marketplace"
+    const val featureMediaPlayerNamespace = "$appNamespace.feature_mediaplayer"
+    const val featureNotificationsNamespace = "$appNamespace.feature_notifications"
+    const val featurePaymentsNamespace = "$appNamespace.feature_payments"
+    const val featurePropertyNamespace = "$appNamespace.feature_property"
+    const val featureServiceNamespace = "$appNamespace.feature_service"
+    const val localizationNamespace = "$appNamespace.feature_localization"
+    const val securityNamespace = "$appNamespace.feature_security"
+}
---
+++ build-logic/convention/src/main/kotlin/SonarQubeConfig.kt
@@ -0,0 +1,26 @@
+/**
+ * Configuration object for SonarQube analysis.
+ *
+ * This object provides constant values and computed properties required for
+ * configuring and executing SonarQube code analysis on the project.
+ */
+@Suppress("constPropertyName", "MemberVisibilityCanBePrivate")
+object SonarQubeConfig {
+    const val projectKey = "harrykibet_RealEstateApp2"
+    const val organization = "harrykibet"
+    const val hostUrl = "https://sonarcloud.io"
+    const val sourceEncoding = "UTF-8"
+    const val sources = "src/main"
+    const val androidVariant = "release"
+    const val exclusions = "**/build/**, **/generated/**"
+    const val tests = "src/test"
+
+    val javaBinaries: String
+        get() = "build/intermediates/javac/release/classes"
+
+    val junitReportPaths: String
+        get() = "build/test-results/testDebugUnitTest"
+
+    val jacocoReportPaths: String
+        get() = "build/reports/jacoco/testDebugUnitTestCoverage/testDebugUnitTestCoverage.xml"
+}
