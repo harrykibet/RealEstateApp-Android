29cd661973828db9d833d4928afcb0d713fd661f
harry <trmnjames@gmail.com>
2026-02-03 20:32:12 +0200

    new changes

--- core/interfaces/src/main/java/com/application/real_estate_app/core_interface/IBatteryManager.kt
+++
@@ -1,8 +0,0 @@
-package com.application.real_estate_app.core_interface
-
-interface IBatteryManager {
-    fun shouldThrottlePerformance(): Boolean
-    fun getRecommendedQualityLevel(maxQuality: Int): Int
-    fun scheduleBackgroundTask(task: Runnable, delay: Long)
-    fun cleanup()
-}
