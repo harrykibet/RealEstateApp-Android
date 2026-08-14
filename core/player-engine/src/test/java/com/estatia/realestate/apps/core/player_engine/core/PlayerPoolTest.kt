73e81a56bf89055a9182e200bff28dcba1bdfee1
harry <trmnjames@gmail.com>
2026-08-14 18:59:29 +0000

    chore(sync): auto update 2026-08-14 18:59:28

--- core/player-engine/src/test/java/com/estatia/realestate/apps/core/player_engine/core/PlayerPoolTest.kt
+++ core/player-engine/src/test/java/com/estatia/realestate/apps/core/player_engine/core/PlayerPoolTest.kt
@@ -45,7 +45,7 @@ class PlayerPoolTest {
         mockkStatic(Looper::class)
         val mockLooper = mockk<Looper>(relaxed = true)
         every { mockLooper.thread } returns Thread.currentThread()
-        every { mockLooper.getThread() } returns Thread.currentThread()
+        every { mockLooper.thread } returns Thread.currentThread()
         every { Looper.getMainLooper() } returns mockLooper
         every { Looper.myLooper() } returns mockLooper
 
